906991435641b707c6a583d53a98c879f0b32f05
Sachini <sachichathudissanayaka@gmail.com>
2024-10-22 18:05:21 +0530

    Add changes

--- react-basics/build-a-react-info-site/custom-components/src/App.jsx
+++ react-basics/build-a-react-info-site/custom-components/src/App.jsx
@@ -1,14 +1,12 @@
 import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
 function App() {
   return (
     <>
       <Header />
-      <h1>Reasons I&apos;m excited to learn React</h1>
-      <ol>
-        <li>It&apos;s a popular library, so I&apos;ll be able to fit in with the cool kids!</li>
-        <li>I&apos;m more likely to get a job as a developer if I know React</li>
-      </ol>
-      <footer>© 2024 s4ch1 development. All rights reserved.</footer>
+      <MainContent />
+      <Footer />
     </>
   )
 }
---
+++ react-basics/build-a-react-info-site/custom-components/src/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer(){
+    return <footer>© 2024 s4ch1 development. All rights reserved.</footer>
+}
+
+export default Footer
