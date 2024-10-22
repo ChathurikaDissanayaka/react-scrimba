68e858b3e3236eb29c39ef37fdfcd5feaa7673c4
Sachini <sachichathudissanayaka@gmail.com>
2024-10-22 17:49:25 +0530

    Add changes

---
+++ react-basics/build-a-react-info-site/custom-components/src/App.jsx
@@ -0,0 +1,20 @@
+import reactLogo from "./assets/react-logo.png"
+function App() {
+  return (
+    <>
+      <header>
+        <nav>
+          <img src={reactLogo} width="40px" />
+        </nav>
+      </header>
+      <h1>Reasons I&apos;m excited to learn React</h1>
+      <ol>
+        <li>It&apos;s a popular library, so I&apos;ll be able to fit in with the cool kids!</li>
+        <li>I&apos;m more likely to get a job as a developer if I know React</li>
+      </ol>
+      <footer>© 2024 s4ch1 development. All rights reserved.</footer>
+    </>
+  )
+}
+
+export default App
