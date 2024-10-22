93c7e160d46de3c214067b5c26b9d753dbe07c07
Sachini <sachichathudissanayaka@gmail.com>
2024-10-22 12:26:17 +0530

    Add changes

---
+++ react-basics/build-a-react-info-site/react-info-site/src/App.jsx
@@ -0,0 +1,19 @@
+import reactLogo from './assets/react-logo.png'
+
+function App() {
+  return (
+    <>
+      <img src={reactLogo} width="40px" />
+      <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ul>
+            <li>It&apos;s composable</li>
+            <li>It&apos;s declarative</li>
+            <li>It&apos;s a hireable skill</li>
+            <li>It&apos;s actively maintained by skilled people</li>
+        </ul>
+    </>
+  )
+}
+
+export default App
