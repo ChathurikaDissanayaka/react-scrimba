251f1c4b720463d1120fa3e00e4f6f435f55e451
Sachini <sachichathudissanayaka@gmail.com>
2024-11-01 23:51:48 +0530

    Add changes

--- react-basics/cat-contacts/src/components/Contact.jsx
+++ react-basics/cat-contacts/src/components/Contact.jsx
@@ -2,21 +2,38 @@
 import PhoneIcon from "../assets/phone-icon.png";
 import MailIcon from "../assets/mail-icon.png";
 
-const Contact = ({img, name, phone, email}) => {
+const Contact = (props) => {
   return (
     <div className="contact-card">
-      <img src={img} />
+      <img src={props.img} />
       <h3>{name}</h3>
       <div className="info-group">
         <img src={PhoneIcon} />
-        <p>{phone}</p>
+        <p>{props.phone}</p>
       </div>
       <div className="info-group">
         <img src={MailIcon} />
-        <p>{email}</p>
+        <p>{props.email}</p>
       </div>
     </div>
   );
 };
+
+// const Contact = ({img, name, phone, email}) => {
+//   return (
+//     <div className="contact-card">
+//       <img src={img} />
+//       <h3>{name}</h3>
+//       <div className="info-group">
+//         <img src={PhoneIcon} />
+//         <p>{phone}</p>
+//       </div>
+//       <div className="info-group">
+//         <img src={MailIcon} />
+//         <p>{email}</p>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Contact;
