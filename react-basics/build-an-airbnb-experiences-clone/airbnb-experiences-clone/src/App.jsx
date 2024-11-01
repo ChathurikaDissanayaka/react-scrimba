97ea25ce7c37e3a2e0b19e9c5b55da18adf329d7
Sachini <sachichathudissanayaka@gmail.com>
2024-11-02 00:21:50 +0530

    Add changes

--- react-basics/build-an-airbnb-experiences-clone/airbnb-experiences-clone/src/App.jsx
+++ react-basics/build-an-airbnb-experiences-clone/airbnb-experiences-clone/src/App.jsx
@@ -2,13 +2,21 @@ import './App.css'
 import Card from './components/Card'
 // import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import katie from './assets/images/katie-zaferes.png'
 
 function App() {
   return (
     <>
       <Navbar />
       {/* <Hero /> */}
-      <Card />
+      <Card
+        img={katie}
+        rating="5.0"
+        reviewCount={6}
+        country="SL"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </>
   )
 }
--- react-basics/build-an-airbnb-experiences-clone/airbnb-experiences-clone/src/components/Card.jsx
+++ react-basics/build-an-airbnb-experiences-clone/airbnb-experiences-clone/src/components/Card.jsx
@@ -1,21 +1,34 @@
-import katie from '../assets/images/katie-zaferes.png'
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import star from '../assets/images/star.png'
 import '../index.css'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
-        <img src={katie} className="card-image" alt="Main card image." />
+  <img src={props.img} className="card-image" alt="Main card image." />
         <div className="card-stats">
             <img src={star} className="card-star" alt="Star icon." />
-            <span>5.0</span>
-            <span className="gray">(6) • </span>
-            <span className="gray">USA</span>
+            <span>{props.rating}</span>
+            <span className="gray">({props.reviewCount}) • </span>
+            <span className="gray">{props.country}</span>
         </div>
-        <h2>Life Lessons with Katie Zaferes</h2>
-        <p><span className="bold">From $136</span> / person</p>
+        <h2>{props.title}</h2>
+        <p><span className="bold">From ${props.price}</span> / person</p>
     </div>
   )
 }
 
 export default Card
+
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
