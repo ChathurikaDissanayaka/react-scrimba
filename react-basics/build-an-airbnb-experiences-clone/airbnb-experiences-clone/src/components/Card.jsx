6243c2465849ee8a2e05dfa097eb23b52a5b44f2
Sachini <sachichathudissanayaka@gmail.com>
2024-11-12 19:27:16 +0530

    Add changes

--- react-basics/build-an-airbnb-experiences-clone/airbnb-experiences-clone/src/components/Card.jsx
+++ react-basics/build-an-airbnb-experiences-clone/airbnb-experiences-clone/src/components/Card.jsx
@@ -1,26 +1,40 @@
 /* eslint-disable react/prop-types */
-import '../index.css'
+import "../index.css";
 
-const Card = (props) => {
+const Card = ({item}) => {
+  let badgeText
+    if (item.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (item.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    
   return (
     <div className="card">
-  <img src={`./images/${props.img}`} className="card-image" alt="Main card image." />
-        <div className="card-stats">
-            <img src="./images/star.png" className="card-star" alt="Star icon." />
-            <span>{props.rating}</span>
-            <span className="gray">({props.reviewCount}) • </span>
-            <span className="gray">{props.location}</span>
-        </div>
-        <h2>{props.title}</h2>
-        <p><span className="bold">From ${props.price}</span> / person</p>
+      <img
+        src={`./images/${item.coverImg}`}
+        className="card-image"
+        alt="Main card image."
+      />
+      {badgeText && <div className="card-badge">{badgeText}</div>}
+      <div className="card-stats">
+        <img src="./images/star.png" className="card-star" alt="Star icon." />
+        <span>{item.stats.rating}</span>
+        <span className="gray">({item.stats.reviewCount}) • </span>
+        <span className="gray">{item.location}</span>
+      </div>
+      <h2>{item.title}</h2>
+      <p>
+        <span className="bold">From ${item.price}</span> / person
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
 
 /*
-Challenge: Pass props to the Card component and display that data
+Challenge: Pass item to the Card component and display that data
 
 - img ("katie-zaferes.png")
 - rating ("5.0")
