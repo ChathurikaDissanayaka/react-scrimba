d46ce70b8a357cc51e7bbe760ba1e72bcf5129ca
Sachini <sachichathudissanayaka@gmail.com>
2024-11-14 19:57:32 +0530

    Add changes

--- react-basics/build-a-meme-generator/src/components/Meme.jsx
+++ react-basics/build-a-meme-generator/src/components/Meme.jsx
@@ -1,28 +1,44 @@
+import { useState } from "react";
+import memesData from "../data/MemesData.js";
+
 const Meme = () => {
+  const [memeImage, setMemeImage] = useState("");
+
+  const getMeme = () => {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  };
+
   return (
-    <div className="form">
-      <div className="inputs">
-        <div className="input">
-          <label htmlFor="top-text">Top text</label>
-          <input
-            type="text"
-            name="top-text"
-            id="top-text"
-            placeholder="Shut up"
-          />
+    <main>
+      <div className="form">
+        <div className="inputs">
+          <div className="input">
+            <label htmlFor="top-text">Top text</label>
+            <input
+              type="text"
+              name="top-text"
+              id="top-text"
+              placeholder="Shut up"
+            />
+          </div>
+          <div className="input">
+            <label htmlFor="bottom-text">Bottom text</label>
+            <input
+              type="text"
+              name="bottom-text"
+              id="bottom-text"
+              placeholder="And take my money"
+            />
+          </div>
         </div>
-        <div className="input">
-          <label htmlFor="bottom-text">Bottom text</label>
-          <input
-            type="text"
-            name="bottom-text"
-            id="bottom-text"
-            placeholder="And take my money"
-          />
-        </div>
+        <button type="submit" onClick={getMeme}>
+          Get a new meme image
+        </button>
       </div>
-      <button type="submit">Get a new meme image</button>
-    </div>
+      <img className="meme" src={memeImage} alt="" />
+    </main>
   );
 };
 
