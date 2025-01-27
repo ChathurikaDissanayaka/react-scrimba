558430774a4d5ea89d40b4e05610aa46f78914b7
Sachini <sachichathudissanayaka@gmail.com>
2025-01-27 19:14:04 +0530

    Add changes

--- react-basics-updated/data-driven-react/travel-journal/src/components/Header.jsx
+++ react-basics-updated/data-driven-react/travel-journal/src/components/Header.jsx
@@ -2,7 +2,7 @@ import globe from "../assets/globe.png";
 const Header = () => {
   return (
     <header>
-      <img src={globe} alt="" />
+      <img className="logo" src={globe} alt="globe logo" />
       <h1>my travel journal.</h1>
     </header>
   );
