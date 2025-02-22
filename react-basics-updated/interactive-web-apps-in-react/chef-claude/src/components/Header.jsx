809287bb9393e99ef8874804189207089b1c8ba7
Sachini <sachichathudissanayaka@gmail.com>
2025-02-22 20:51:19 +0530

    Add changes

---
+++ react-basics-updated/interactive-web-apps-in-react/chef-claude/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import chefClaudeIcon from "../assets/chef-claude-icon.png"
+const Header = () => {
+  return (
+    <div className="header">
+        <img src={chefClaudeIcon} alt="chef claude icon" />
+        <h1>Chef Claude</h1>
+    </div>
+  )
+}
+
+export default Header
