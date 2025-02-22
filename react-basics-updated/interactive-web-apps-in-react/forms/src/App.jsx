c01dd72e9d226f55ea0555aa50e641cdc6a3988f
Sachini <sachichathudissanayaka@gmail.com>
2025-02-22 22:39:53 +0530

    Add changes

--- react-basics-updated/interactive-web-apps-in-react/forms/src/App.jsx
+++ react-basics-updated/interactive-web-apps-in-react/forms/src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+    <section>
+      <h1>Signup form</h1>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="joe@schmoe.com"
+        />
+        <br />
+
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password" />
+        <br />
+
+        <button>Submit</button>
+      </form>
+    </section>
+  );
 }
 
-export default App
+export default App;
