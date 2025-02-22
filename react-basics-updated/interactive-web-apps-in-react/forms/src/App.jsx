732f029cf2f79f6f1162c5d9526e51150c320d9e
Sachini <sachichathudissanayaka@gmail.com>
2025-02-22 22:44:50 +0530

    Add changes

--- react-basics-updated/interactive-web-apps-in-react/forms/src/App.jsx
+++ react-basics-updated/interactive-web-apps-in-react/forms/src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
 
 function App() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formEl = event.currentTarget;
+    const formData = new FormData(formEl);
+    const email = formData.get("email");
+    formEl.reset();
+  }
+
   return (
     <section>
       <h1>Signup form</h1>
-      <form>
+      <form onSubmit={handleSubmit} method="post">
         <label htmlFor="email">Email:</label>
         <input
           id="email"
@@ -13,11 +21,9 @@ function App() {
           placeholder="joe@schmoe.com"
         />
         <br />
-
         <label htmlFor="password">Password:</label>
         <input id="password" type="password" name="password" />
         <br />
-
         <button>Submit</button>
       </form>
     </section>
