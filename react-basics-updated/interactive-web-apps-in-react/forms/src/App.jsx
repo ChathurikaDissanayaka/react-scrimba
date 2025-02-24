824bd1adf82ab1d8f05e602de332d7fd8713b489
Sachini <sachichathudissanayaka@gmail.com>
2025-02-24 21:14:25 +0530

    Add changes

--- react-basics-updated/interactive-web-apps-in-react/forms/src/App.jsx
+++ react-basics-updated/interactive-web-apps-in-react/forms/src/App.jsx
@@ -4,7 +4,8 @@ const App = () => {
   const signUp = (formData) => {
     const email = formData.get("email");
     const password = formData.get("password");
-    console.log(email, password);
+    const employmentStatus = formData.get("employmentStatus");
+    console.log(email, password, employmentStatus);
   };
 
   return (
@@ -26,6 +27,27 @@ const App = () => {
 
         <label htmlFor="description">Description:</label>
         <textarea id="description" name="description"></textarea>
+
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label>
+            <input type="radio" name="employmentStatus" value="unemployed" />
+            Unemployed
+          </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="part-time" />
+            Part-time
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="employmentStatus"
+              defaultChecked
+              value="full-time"
+            />
+            Full-time
+          </label>
+        </fieldset>
 
         <button>Submit</button>
       </form>
